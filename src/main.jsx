63140d7e69b90d8570e4cde57ac8ef3064c813f0
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-10-06 09:42:50 -0400

    Incorporacion de ruteo

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import vechicles from './data/vehicles.js';
+
+const routes = [
+  { path: "/",
+    element: <App />,
+  }];
+vechicles.forEach((v)=>{
+  routes.push({
+    path: "/"+v.name,
+    element: <div>{v.name}</div>,
+  })
+})
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
+
